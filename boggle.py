f29f8734e0a047a6f6d66a7ee480903ac4f9dd6a
Melanie Milsted <melanie.milsted@gmail.com>
2018-09-23 10:03:24 +0000

    enables program to iterate through the eight potential neighboring positions on the grid

--- boggle.py
+++ boggle.py
@@ -1,8 +1,52 @@
+from string import ascii_uppercase
+from random import choice
+
+
 def make_grid(width, height):
     """
    Creates a grid that will hold all of the tiles
    for a boggle game
     """
-    return {(row, col): ' ' for row in range(height)
+    return {(row, col): choice(ascii_uppercase)
+        for row in range(height)
         for col in range(width)}
+    
+    
+def neighbours_of_position(coords):
+    """
+    Get neighbours of a given position
+    """
+    row = coords[0]
+    col = coords[1]
+    
+    #Assign each of the neighbours
+    #Top-left to Top-right.
+    top_left = (row - 1, col - 1)
+    top_center = (row - 1, col)
+    top_right = (row - 1, col + 1)
+    
+    # Left to right
+    left = (row, col - 1)
+    # The `(row, col)` coordanites passed to this
+    # function are situated here
+    right = (row, col + 1)
+    
+    #Bottom-left to bottom-right
+    bottom_left = (row + 1, col - 1)
+    bottom_center = (row + 1, col)
+    bottom_right = (row + 1, col + 1)
+    return [top_left, top_center, top_right,
+            left, right,
+            bottom_left, bottom_center, bottom_right]
+    
+def all_grid_neighbours(grid):
+        """
+        Gets all of the possible neighbours for each position
+        in the grid
+        """
+        neighbours = {}
+        for position in grid:
+            position_neighbours = neighbours_of_position(position)
+            neighbours[position] = [p for p in position_neighbours if p in grid]
+        return neighbours
     
--- test_boggle.py
+++ test_boggle.py
@@ -1,5 +1,6 @@
 import unittest
 import boggle
+from string import ascii_uppercase
 
 
 class TestBoggle(unittest.TestCase):
@@ -37,3 +38,53 @@ class TestBoggle(unittest.TestCase):
         self.assertNotIn((2, 2), grid)
         
         
+    def test_grid_is_filled_with_letters(self):
+        """
+        Ensure that each of the coordinates in the grid
+        contains letters
+        """
+        grid = boggle.make_grid(2, 3)
+        for letter in grid.values():
+            self.assertIn(letter, ascii_uppercase)
+            
+            
+    def test_neighbours_of_a_position(self):
+        """
+        Ensure that a position has 8 neighbours
+        """
+        coords = (1, 2)
+        neighbours = boggle.neighbours_of_position(coords)
+        self.assertIn((0, 1), neighbours)
+        self.assertIn((0, 2), neighbours)
+        self.assertIn((0, 3), neighbours)
+        self.assertIn((1, 1), neighbours)
+        self.assertIn((1, 3), neighbours)
+        self.assertIn((2, 1), neighbours)
+        self.assertIn((2, 2), neighbours)
+        self.assertIn((2, 3), neighbours)
+        
+        
+    def test_all_grid_neighbours(self):
+        """
+        Ensure all grid positions have neighbours
+        """
+        grid = boggle.make_grid(2, 2)
+        neighbours = boggle.all_grid_neighbours(grid)
+        self.assertEqual(len(neighbours), len(grid))
+        for  pos in grid:
+            others = list(grid) # creates a new list from the dictionary's keys
+            others.remove(pos)
+            self.assertListEqual(sorted(neighbours[pos]), sorted(others))
+        
+        
+        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
